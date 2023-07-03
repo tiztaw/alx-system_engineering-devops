2ed4561b8145b3fc22eb8a8833b09dbcd6bfce20
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-07-03 13:49:28 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,35 @@
-# Use Puppet to automate the task of creating a custom HTTP header response
-
-exec {'update':
+# puppet manifest creating a custom HTTP header response
+exec { 'apt-get-update':
   command => '/usr/bin/apt-get update',
 }
--> package {'nginx':
-  ensure => 'present',
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
--> exec {'run':
-  command => '/usr/sbin/service nginx restart',
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
